5e310aeb8226b30d93e1d1a978a4c263bb73a8d7
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-03 15:48:02 +0100

    add solution

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ console.clear();
 // Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "Nadine",
+  age: "32",
+  email: "ichbineine@mail.com",
 };
+
+console.log("EXERCISE 1: ", personWithNameAgeEmail);
 
 // EXERCISE 2
 // Set the values of name and age equal to the corresponding values of the personAlex object.
@@ -15,8 +19,10 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
+
+console.log("EXERCISE 2: ", nameOfAlex, ageOfAlex);
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
@@ -26,6 +32,10 @@ const personToChange = {
   age: 24,
 };
 
+personToChange.age = 35;
+personToChange.name = "Alex";
+console.log("EXERCISE 3: ", personToChange);
+
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
 
@@ -33,6 +43,11 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+
+petPluto.age = 5;
+petPluto.breed = "husky";
+
+console.log("EXERCISE 4: ", petPluto);
 
 export {
   personWithNameAgeEmail,
